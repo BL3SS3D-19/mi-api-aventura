15cf2dd01ebdb82c64355491a82d95742d1719ae
BL3SS3D-19 <peibol1912@gmail.com>
2026-01-29 20:51:45 +0100

    Eliminacion de archivos sensibles

--- api/v1/hola.js
+++ api/v1/hola.js
@@ -1,8 +1,14 @@
+/*
 export function handler(req, res) {
     res.status(200).json({ message: process.env.MENSAJE_BIENVENIDA, version: '1.0.0' })
 }
 
+*/
+
 export function handlerWparam(req, res) {
     const nombre = req.params.nombre;
-    res.status(200).json({ message: 'Bienvenido a mi API, ' + nombre })
+    if (!nombre) {
+        return res.status(400).json({ message: 'El nombre es requerido' })
+    }
+    return res.status(200).json({ message: 'Bienvenido a mi API, ' + nombre })
 }
