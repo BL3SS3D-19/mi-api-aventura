4ac0dcffac4ef74a3ff2ee522480b974ccff0d3d
BL3SS3D-19 <peibol1912@gmail.com>
2026-01-29 17:44:41 +0100

    Actualizamos un endpoint GET con parametro

--- api/v1/hola.js
+++ api/v1/hola.js
@@ -1,3 +1,8 @@
 export default function handler(req, res) {
     res.status(200).json({ message: process.env.MENSAJE_BIENVENIDA, version: '1.0.0' })
 }
+
+export default function handlerWparam(req, res) {
+    const nombre = req.params.nombre;
+    res.status(200).json({ message: 'Bienvenido a mi API, ' + nombre })
+}
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import handler from './api/v1/hola.js';
+import { handler, handlerWparam } from './api/v1/hola.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/api/v1/hola', handler);
+
+app.get('/api/v1/hola/:nombre', handlerWparam);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
