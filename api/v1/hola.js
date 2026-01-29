04e43660976d6edcaa71e8503ff3b89f5a115e27
BL3SS3D-19 <peibol1912@gmail.com>
2026-01-29 17:53:38 +0100

    ...

--- api/v1/hola.js
+++ api/v1/hola.js
@@ -1,8 +1,8 @@
-export default function handler(req, res) {
+export function handler(req, res) {
     res.status(200).json({ message: process.env.MENSAJE_BIENVENIDA, version: '1.0.0' })
 }
 
-export default function handlerWparam(req, res) {
+export function handlerWparam(req, res) {
     const nombre = req.params.nombre;
     res.status(200).json({ message: 'Bienvenido a mi API, ' + nombre })
 }
