cb14e2a65595059de3f583488861bac4354bbb78
BL3SS3D-19 <peibol1912@gmail.com>
2026-02-05 19:27:04 +0100

    Eliminacion de archivos sensibles

--- api/v1/usuarios.js
+++ api/v1/usuarios.js
@@ -3,7 +3,7 @@ import Usuario from "../../models/users.js";
 
 export async function getUsuarios(req, res) {
     try {
-        const usuarios = await Usuario.find();
+        let usuarios = await Usuario.find();
         res.status(200).json({ usuarios });
     } catch (error) {
         res.status(500).json({ error: error.message });
